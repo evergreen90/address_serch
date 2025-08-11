8ba34ae6e5151b743a6db32d3cf993c2f2badfdb
evergreen90 <evergreen90@gmail.com>
2025-08-11 16:57:53 +0900

    受付入力ができた

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-# adress 
+import requests
+
+zipcode = input("郵便番号を入力してください:")
+response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}")
+
+print(response)
+print(response.text)
+
+
