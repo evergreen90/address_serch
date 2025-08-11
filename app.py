3527cda4f72bbb3285c55a822f211c24193238f1
evergreen90 <evergreen90@gmail.com>
2025-08-11 17:42:58 +0900

    できた

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+from types import CoroutineType
 import requests
+from requests.sessions import preferred_clock
+from requests.utils import address_in_network
 
 zipcode = input("郵便番号を入力してください:")
 response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}")
 
-print(response)
-print(response.text)
+adress_info = response.json()["results"][0]
+
+pref = adress_info["address1"]
+city = adress_info["address2"]
+area = adress_info["address3"]
 
 
+
+print(f"{pref}{city}{area}")
